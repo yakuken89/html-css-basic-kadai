c6fa165e1d8fda422751d783fde13ecc15f79c97
yakuken89 <yakuken8989@gmail.com>
2026-05-23 17:55:09 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,15 @@
+let num = 15;
+
+if (num ％ ３ == 0 ) {
+  console.log('3の倍数です');
+}
+
+if (num ％ 5 == 0 ) {
+  console.log('５の倍数です');
+}
+
+if (num ％ ３ == 0 && num ％ 5 == 0 ) {
+    console.log('3と5の倍数です');
+} else {
+    console.log('num');}
+    
